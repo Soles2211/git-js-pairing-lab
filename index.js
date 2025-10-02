dd6454082d59461ad2918367f94e27643b853f92
Solea Miller <soleamiller@gmail.com>
2025-10-02 14:42:11 -0400

    first function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //Code your solutions in this file
+// Write a function fiveToOneHundred that console logs all numbers from 5 to 100.
+const fiveToOneHundred = () => {
+    for (let i = 5; i <= 100; i++){
+        console.log(i);
+    }
+}
+
+fiveToOneHundred();
